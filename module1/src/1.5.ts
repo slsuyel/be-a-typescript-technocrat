b8e421688d2d041afaa7420bbfa7a83b5fdfa796
Suyel Haque <slsuyel@gmail.com>
2023-11-03 17:33:30 +0600

    optional type,readonly litarel type,

---
+++ module1/src/1.5.ts
@@ -0,0 +1,16 @@
+//reference type
+
+const user: {
+    readonly country: 'Bangladesh' //litarel type  //readonly
+    firstName: string;
+    middleName?: string // ? mark is optional type
+    lastName: string;
+    isMarrid?: boolean;
+} = {
+    country: "Bangladesh",
+    firstName: 'Suyel',
+    lastName: " Haque",
+    isMarrid: true
+}
+
+
