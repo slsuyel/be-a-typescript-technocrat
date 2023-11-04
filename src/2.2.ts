3b2957bf92c77190a37581c243b701390d1d7bf4
Suyel Haque <slsuyel@gmail.com>
2023-11-04 21:51:36 +0600

    generic type start

--- src/2.2.ts
+++ src/2.2.ts
@@ -1,42 +1,49 @@
 {
+    // interface
 
-    //interface
-
-
-
-    /* type */
     type User1 = {
+        name: string;
+        age: number;
+    };
+
+    interface User2 {
         name: string;
         age: number;
     }
 
+    type UserWithRole1 = User1 & { role: string };
 
-    const user1: User1 = {
-        name: 'Suyel',
-        age: 5
+    interface UserWithRole2 extends User1 {
+        role: string;
     }
 
+    const user1: UserWithRole2 = {
+        name: "Persian",
+        age: 100,
+        role: "manager",
+    };
 
-    /* interface */
+    type rollNumber = number;
 
-    interface User2 {
-        name: string;
-        age: number
+    // js --> object , array-> object function -> object
+
+    type Roll1 = number[];
+
+    interface Roll2 {
+        [index: number]: number
     }
 
+    const rollNumber1: Roll2 = [1, 2, 3]
+    
 
-    const user2: User2 = {
-        name: 'Suyel',
-        age: 15
+
+    type Add1 = (num1: number, num2: number) => number
+
+    interface Add2 {
+        (num1: number, num2: number): number
     }
 
+    const add: Add2 = (num1, num2) => num1 + num2
 
-
-
-
-
-
-
-
-
+    //
 }
