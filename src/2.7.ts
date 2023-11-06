cf1a3e9d17097532220725900882e69e53fe85c3
Suyel Haque <slsuyel@gmail.com>
2023-11-06 21:57:32 +0600

    generic constraints with keyop operator

--- src/2.7.ts
+++ src/2.7.ts
@@ -14,6 +14,51 @@
     type Owner = 'byke' | 'car' | 'ship';      //manually
     type Owner2 = keyof Vehicle
 
+    const getPropeertyValue = <X, Y extends keyof X>(obj: X, key: Y) => {
+        return obj[key]
+    }
+
+    const user = {
+        name: 'Suyel',
+        age: 22,
+        address: 'dhaka'
+    }
+
+    const result = getPropeertyValue(user, 'name')
+
+    const car = {
+        model: 'Toyota 100',
+        year: 2012
+    }
+
+
+    const result2 = getPropeertyValue(car, 'model')
+   
+
+
+
+
+
+    // const user = {
+    //     name: 'Suyel',
+    //     age: 22,
+    //     address: 'dhaka'
+    // }
+
+    // user['name']
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
